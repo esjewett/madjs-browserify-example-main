9ce1f0591a931c2d6c59f52b9f0613d38d44fb26
Ethan Jewett <esjewett@gmail.com>
2014-11-09 10:51:14 -0500

    Adda Github-based dependency

--- example.js
+++ example.js
@@ -1,8 +1,16 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.example=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var dependency = require('madjs-browserify-example-dependency');
+
 function example() {
-	return "This is an example.";
+	return { text: "This is an example.", dependency: dependency };
 }
 
 module.exports = example;
+},{"madjs-browserify-example-dependency":2}],2:[function(require,module,exports){
+function dependency() {
+	return "This is a dependency.";
+}
+
+module.exports = dependency;
 },{}]},{},[1])(1)
 });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+var dependency = require('madjs-browserify-example-dependency');
+
 function example() {
-	return "This is an example.";
+	return { text: "This is an example.", dependency: dependency };
 }
 
 module.exports = example;
